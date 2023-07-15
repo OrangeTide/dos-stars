7d2fe7780cd0c2439e4417fb53f2a691ac3ea202
Jon Mayo <jon@rm-f.net>
2023-07-15 10:56:37 -0700

    fixed hang on exit

--- stars.asm
+++ stars.asm
@@ -2,10 +2,9 @@
 ; Copyright (C) 2020 by Jon Mayo <jon@rm-f.net>
 ; the BSD Zero Clause license
 ;
+include util.inc
 .8086
 .model tiny
-ASSUME DS:_DATA
-ASSUME DS:_BSS
 
 num_stars	equ	40
 _TEXT SEGMENT
@@ -32,19 +31,19 @@ start:
 initialize_stars:
 	mov cx, num_stars
 	mov si, OFFSET stars
-loop1:
+@@:
 	call rand
-	and ax, 07fh		; restrict Y axis to 0-127
+	and ax, 7Fh		; restrict Y axis to 0-127
 	mov dx, 80		; multiply by screen pitch (80 bytes)
 	mul dx
 	mov [si+2], ax		; save star_y[i]
 
 	call rand
-	and ax, 03fh		; restrict X axis to 0-63
+	and ax, 3Fh		; restrict X axis to 0-63
 	mov [si+0], ax		; save star_x[i]
 
 	add si, 4		; 2+2 ; record size of stars[]
-	loop loop1
+	loop @B
 	ret
 do_frame:
 
@@ -98,7 +97,6 @@ delay:
 
 	ret
 
-
 wait_retrace:
 ;; TODO: do a better job and wait for a 0->1 transition
 	mov dx, 03dah	; video status register
@@ -138,8 +136,7 @@ restore_mode:
 	jmp _set_mode
 goodbye:
 	mov dx, OFFSET goodbye_msg
-	mov ah, 09h	; INT 21h, 09h : Write string to STDOUT
-	int 21h
+	dosint 09h	; INT 21h, 09h : Write string to STDOUT
 	int 20h
 srand:
 	mov WORD PTR [rand_seed], ax
@@ -154,10 +151,10 @@ rand:
 	push bx
 	push cx
 
-	mov ax, 043fdh		; lower word of 214013
+	mov ax, 043FDh		; lower word of 214013
 	mov bx, WORD PTR [rand_seed]	; load lower word of rand_seed
 	imul WORD PTR [rand_seed]
-	add ax, 09ec3h		; lower word of 2531011
+	add ax, 09EC3h		; lower word of 2531011
 	mov WORD PTR [rand_seed], ax	; dx:ax contains first step, save lower word
 	mov cx, dx		; save dx from first step
 
@@ -168,26 +165,22 @@ rand:
 	add dx, cx		; add dx from second step
 
 	add dx, 00026h		; upper word of 2531011
-	and dx, 07fffh		; upper word of 0x7fffffff
+	and dx, 07FFFh		; upper word of 0x7fffffff
 	mov WORD PTR [rand_seed + 2], dx	; save upper word
 
 	pop cx
 	pop bx
 
 	ret
-_TEXT ENDS
-_DATA SEGMENT
-	press_msg db 'Press Any Key ...$'
-	goodbye_msg db 'Thank you!'
-	crlf db 13, 10,'$'
-_DATA ENDS
-_BSS SEGMENT
+
+	press_msg db 'Press Any Key ...', '$'
+	goodbye_msg db 'Thank you!', '$'
 	rand_seed	dd	0
 	saved_mode	db	0
 	video_seg	dw	0
-	stars		db	((2+2)*num_stars) DUP(0)
+	stars		db	((2 + 2) * num_stars) DUP(0)
 				; +0	star_x
 				; +2	star_y
-	; TODO: star_xrate
-_BSS ENDS
+				; TODO: star_xrate
+_TEXT ENDS
 END
