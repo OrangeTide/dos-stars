6e17d90d9aa0952e4cceb20ac88f77a8903b4037
Jon Mayo <jon@rm-f.net>
2023-07-15 13:18:38 -0700

    more sophesticated CGA pixel drawing routines

--- randpix.asm
+++ randpix.asm
@@ -1,5 +1,5 @@
-; stars.asm - draw animated stars on DOS 16-bit real mode (assembler)
-; Copyright (C) 2020 by Jon Mayo <jon@rm-f.net>
+; randpix.asm - fill the screen with random pixel in CGA mode
+; Copyright (C) 2020-2023 by Jon Mayo <jon@rm-f.net>
 ; the BSD Zero Clause license
 ;
 include util.inc
@@ -23,22 +23,35 @@ main:
 fillrand:
 	push es
 	mov es, [video_seg]		; we'll use ES for our graphics plotting
-	xor bx, bx
-	xor si, si
+	xor bx, bx			; row position
 nextline:				; for each row ...
-	mov cx, 80			; 80 bytes = 320 pixels
-	; todo calculate row address and load into BX
-@@:					; for each column ...
-	call rand
-	mov es:[si], al			; put down 4 pixels on the even row
-	call rand
-	mov es:[si+2000h], al		; put down 4 pixels on the odd row
+	; calculate row address and load into DI
+	xor di, di
+	mov ax, bx
+	; pick odd or even row. set di to 8192 if (ax & 1) is set
+	shr ax, 1
+	sbb di, 0			; if C set, underflow to FFFFh, else 0
+	and di, 2000h			; only save the upper bit at 8192
+	; di = ax << 4 + ax << 2  ... ax * 20
+	shl ax, 1
+	shl ax, 1
+	shl ax, 1
+	shl ax, 1
+	add di, ax
+	shl ax, 1
+	shl ax, 1
+	add di, ax
 
-	inc si
-
+	mov cx, 80			; process 80 bytes per row = 320 pixels
+@@:					; do for each pixel in row ...
+	call rand
+	stosb				; put down 4 pixels on the selected row
+					; equivalent to:
+					; mov es:[di], al ...
+					; inc di
 	loop @B
-	inc bx				; we processed 2 rows
-	inc bx
+
+	inc bx				; next row
 	cmp bx, 200
 	jle nextline
 
